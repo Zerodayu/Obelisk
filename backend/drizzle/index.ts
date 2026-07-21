3af6a450f0f7dbb62c8761817b9c72cbe4f18940
Zerodayu <kimianalconera39@gmail.com>
2026-07-22 02:26:02 +0800

    update: change ORM from `drizzle` to `prismaORM`

--- backend/drizzle/index.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "@neondatabase/serverless";
-import { env } from "@utils/env";
-import { drizzle } from "drizzle-orm/neon-serverless";
-
-const pool = new Pool({ connectionString: env.DATABASE_URL });
-
-export const db = drizzle({ client: pool });
-
-export { db as default };
