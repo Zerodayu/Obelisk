d64cfdeeaa3b375ce97fdbce40f7b2237607c95e
Zerodayu <kimianalconera39@gmail.com>
2026-08-05 02:37:10 +0800

    update: add forms and ingest routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,9 @@
 import Elysia from "elysia";
 import { authPlugin } from "./v1/auth/controller";
+import { formsPlugin } from "./v1/forms/controller";
+import { ingestPlugin } from "./v1/ingest/controller";
 
-export const apiRoutesV1 = new Elysia({ prefix: "api/v1" }).use(authPlugin);
+export const apiRoutesV1 = new Elysia({ prefix: "api/v1" })
+	.use(authPlugin)
+	.use(formsPlugin)
+	.use(ingestPlugin);
