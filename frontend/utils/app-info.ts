2119b30427f4f23b23641c9ee8af1f600ad6e11f
Zerodayu <kimianalconera39@gmail.com>
2026-08-07 03:13:06 +0800

    new: added global constant for app info

---
+++ frontend/utils/app-info.ts
@@ -0,0 +1,14 @@
+export const app = {
+  title: "Obelisk",
+  version: "0.1.0",
+  description:
+    "Outcomes-based Educational Learning and Intelligent System Kit for Jose Maria College Foundation Inc.",
+  legalTitle:
+    "Obelisk – Outcomes-based Educational Learning and Intelligent System Kit for Jose Maria College Foundation Inc.",
+  organization: "Jose Maria College Foundation Inc.",
+  organizationAbbr: "JMCFI",
+  logo: {
+    light: "/metadata/obelisk-logo.svg",
+    dark: "/metadata/obelisk-logo-dark.svg",
+  },
+} as const;
